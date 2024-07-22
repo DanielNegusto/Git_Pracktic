b283ffd850f512f98635bea502394d55db5aa454
Даниил Ефремов <de312451@gmail.com>
2024-07-22 13:02:11 +0300

    Update

--- src/external_api.py
+++ src/external_api.py
@@ -11,14 +11,16 @@ API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
 API_URL = "https://api.apilayer.com/exchangerates_data/convert"
 
 
-def convert_to_rub(amount: float, currency: str) -> float:
+def convert_to_rub(transaction: Dict[str, Any]) -> float:
     """
     Конвертирует сумму из заданной валюты в рубли.
 
-    :param amount: Сумма в исходной валюте
-    :param currency: Код исходной валюты (например, 'USD', 'EUR')
+    :param transaction: Словарь транзакции
     :return: Сумма в рублях
     """
+    amount = float(transaction["operationAmount"]["amount"])
+    currency = transaction["operationAmount"]["currency"]["code"]
+
     if currency == "RUB":
         return amount
 
@@ -34,15 +36,3 @@ def convert_to_rub(amount: float, currency: str) -> float:
     else:
         error_message = response_data.get("error", "Unknown error")
         raise ValueError(f"Error in API request: {error_message}")
-
-
-def get_transaction_amount_in_rub(transaction: Dict[str, Any]) -> float:
-    """
-    Возвращает сумму транзакции в рублях.
-
-    :param transaction: Словарь с данными транзакции
-    :return: Сумма транзакции в рублях
-    """
-    amount = float(transaction["operationAmount"]["amount"])
-    currency = transaction["operationAmount"]["currency"]["code"]
-    return convert_to_rub(amount, currency)
